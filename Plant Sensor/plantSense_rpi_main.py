9e6219beef39181080ce4344f791081d564d5f7e
James Gallier <jmkgallier@gmail.com>
2020-03-27 17:55:55 -0400

    restructured directory, streamlined bash audio file calls

--- Plant Sensor/plantSense_rpi_main.py
+++ Plant Sensor/plantSense_rpi_main.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 import serial
 import time
 import os
@@ -7,9 +6,7 @@ import RPi.GPIO as GPIO
 
 
 def mainListener(ard_str):
-    # Add LED functions
-    # Call Distance function
-    ard_tup = ard_str.readline().decode("utf-8").replace("\r\n", "").split(" ") #takes the entire message from Arduino and saves
+    ard_tup = ard_str.readline().decode("utf-8").replace("\r\n", "").split(" ")
     moist_val = float(ard_tup[0])
     dist_val = float(ard_tup[1])
     return moist_val, dist_val
@@ -18,7 +15,7 @@ def mainListener(ard_str):
 def distDiscerner(ard_str):
     ard_tup = mainListener(ard_str)
     print(d_param)
-    if d_param > dist_thres:
+    if d_param > distance_threshold:
         waterNotification(m_param)
     else:
         pass
@@ -29,13 +26,12 @@ def ledNotification():
 
 
 def waterNotification(m_param):
-    if m_param <= moist_thres:
+    if m_param <= soil_threshold:
         print(m_param)
-        
         os.system(bash_thirsty)
         userInteraction()
     
-    elif m_param > moist_thres:
+    elif m_param > soil_threshold:
         print(m_param)
         GPIO.output(green_LED,False)
         GPIO.output(red_LED,True)
@@ -49,76 +45,62 @@ def userInteraction():
     m_param = 0
     time_counter = 0
     
-    while time_counter < 15 and m_param < moist_thres: #counts and checks moisture level for 15 times
+    while time_counter < 15 and m_param < soil_threshold: #counts and checks moisture level for 15 times
         time_counter += 1
         ser = serial.Serial('/dev/ttyACM0', 9600, 8, 'N', 1, timeout=3)
         ard_tup = mainListener(ser)
         m_param = ard_tup[0]
         d_param = ard_tup[1]
         
-        if m_param > moist_thres:
+        if m_param > soil_threshold:
             print(m_param)
-            GPIO.output(green_LED,False)
-            GPIO.output(red_LED,False)
+            GPIO.output(green_LED, False)
+            GPIO.output(red_LED, False)
             os.system(bash_watered)
-            GPIO.output(green_LED,True)
-            GPIO.output(red_LED,False)
+            GPIO.output(green_LED, True)
+            GPIO.output(red_LED, False)
             time.sleep(5)
-            GPIO.output(green_LED,False)
-            GPIO.output(red_LED,False)
+            GPIO.output(green_LED, False)
+            GPIO.output(red_LED, False)
             
             break
         else:
             continue
-        
-        
-def distarrPrimer(dist_val, dist_array):
-    if len(dist_array) == 3:
-        del dist_array[0]
-        dist_array.append(dist_val)
-    elif len(dist_array) < 3:
-        dist_array.append(dist_val)
-    elif len(dist_array) > 3:
-        del dist_array[4]
-        distarrPrimer(dist_val, dist_array)
-    return dist_array
 
 
 red_LED = 24
 green_LED = 23
-dist_thres = .15
-moist_thres = .40
+distance_threshold = .15
+soil_threshold = .40
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(red_LED, GPIO.OUT)
 GPIO.setup(green_LED, GPIO.OUT)
 
 ser = serial.Serial('/dev/ttyACM0', 9600, 8, 'N', 1, timeout=3)
-bash_thirsty = "aplay --format=S16_LE --rate=16000 ${HOME}/Downloads/audio(raw)/thirsty.raw"
-bash_satis = "aplay --format=S16_LE --rate=16000 ${HOME}/Downloads/audio(raw)/satisfied_plant.raw"
-bash_watered = "aplay --format=S16_LE --rate=16000 ${HOME}/Downloads/audio(raw)/watered_plant.raw"
+audio_prefix = "aplay --format=S16_LE --rate=16000 " + os.getcwd()
+bash_thirsty = audio_prefix + "/static_audio/thirsty.raw"
+bash_satis = audio_prefix + "/static_audio/satisfied_plant.raw"
+bash_watered = audio_prefix + "/static_audio/watered_plant.raw"
 
 dev_counter = 0
-d_level_arr = []
 time.sleep(4)
 
-while dev_counter < 50:  # Change to While TRUE when ready to deploy device after using the Cron tool
+while dev_counter < 50:
     try:
         ser_tup = mainListener(ser)
         m_level = ser_tup[0]
         d_level = ser_tup[1]
-#         d_level_arr = distarrPrimer(d_level, d_level_arr)
-#         d_level = sum(d_level_arr)/len(d_level_arr)
         print(d_level)
-        if m_level > moist_thres:
+        if m_level > soil_threshold:
             GPIO.output(red_LED, False)
             GPIO.output(green_LED, True)
             continue
-        elif m_level < moist_thres and d_level > dist_thres:
+        elif m_level < soil_threshold and d_level > distance_threshold:
             GPIO.output(green_LED, False)
             GPIO.output(red_LED, True)
             waterNotification(m_level)
-        elif m_level < moist_thres and d_level < dist_thres:
+        elif m_level < soil_threshold and d_level < distance_threshold:
             GPIO.output(green_LED, False)
             GPIO.output(red_LED, False)
             pass
@@ -127,4 +109,3 @@ while dev_counter < 50:  # Change to While TRUE when ready to deploy device afte
         print(dev_counter)
     except IndexError:
         continue
-
