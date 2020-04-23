3a9e2e33ef9e980b061e44ffeb6b417f485c91f2
James Gallier <jmkgallier@gmail.com>
2020-04-23 15:43:14 -0400

    Merged with nixTools updates

--- Setting up Raspbian/GA_RPi_setup.sh
+++ Setting up Raspbian/GA_RPi_setup.sh
@@ -1,27 +1,45 @@
 #!/bin/bash
 
-MIC_CARD=${1}
-MIC_DEVICE=${2}
-SOUND_CARD=${3}
-SOUND_DEVICE=${4}
-CLIENT_SECRET=${5}
+#### Option Input
+while [ -n "${1-}" ]; do
+  case "$1" in
+  -mic) MIC_test="$2"
+    if [[ "$MIC_test" =~ .*",".* ]]; then
+      MIC_address="${MIC_test}"
+      echo "MICROPHONE SET TO: $MIC_address"
+    fi
+    ;;
+  -speaker) SPEAKER_test="$2"
+    if [[ "$SPEAKER_test" =~ .*",".* ]]; then
+      SPEAKER_address="${SPEAKER_test}"
+      echo "SPEAKER SET TO: $SPEAKER_address"
+    fi
+    ;;
+    --client-secret) CLIENT_SECRET_PATH="$2"
+      echo "${CLIENT_SECRET_PATH}"
+    ;;
+  --) shift ; break ;;
+  esac
+  shift
+done
 
 install_GA_DEP() {
-  apt -qq update
-  apt -qq install portaudio19-dev libffi-dev libssl-dev python3-dev python3-venv -y
+  sudo apt -qq update
+  sudo apt -qq install portaudio19-dev libffi-dev libssl-dev python3-dev python3-venv python-serial python3-serial -y
 }
 
 install_GA() {
   python3 -m venv env
   env/bin/python3 -m pip install --upgrade pip setuptools wheel
   source env/bin/activate
-  python3 -m pip install --upgrade google-assistant-sdk[samples]
-  python3 -m pip install --upgrade google-auth-oauthlib[tool]
-  google-oauthlib-tool --scope https://www.googleapis.com/auth/assistant-sdk-prototype --save --headless --client-secrets "${CLIENT_SECRET}"
+  python3 -m pip install --upgrade google-assistant-sdk[samples] google-auth-oauthlib[tool]
+  google-oauthlib-tool --scope https://www.googleapis.com/auth/assistant-sdk-prototype --save --headless --client-secrets "${CLIENT_SECRET_PATH}"
 }
 
-update_asoundrc() {
-  cat "pcm.!default {
+config_Speaker() {
+  rm -rf ~/.asoundrc
+  touch ~/.asoundrc
+  echo "pcm.!default {
   type asym
   capture.pcm 'mic'
   playback.pcm 'speaker'
@@ -29,17 +47,19 @@ update_asoundrc() {
   pcm.mic {
     type plug
     slave {
-      pcm 'hw:${MIC_CARD},${MIC_DEVICE}'
+      pcm 'hw:${MIC_address}'
+      }
     }
-  }
   pcm.speaker {
     type plug
     slave {
-      pcm 'hw:${SOUND_CARD},${SOUND_DEVICE}'
+      pcm 'hw:${SPEAKER_address}'
       rate 16000
     }
-  }" >>/home/pi/.asoundrc
+  }" >> ~/.asoundrc
+  amixer set Master 70%
 }
 
-
-echo "${MIC_CARD}"
+install_GA_DEP
+install_GA
+config_Speaker
