c12ccca194874f5d649faddf49d7e8ce898ce8ab
Assistant-Integration RPi <jmkgallier@gmail.com>
2020-05-04 22:22:28 -0400

    initiated Serial duplexing between virtual RPI and hw Ard

--- Plant Sensor/plantSense_ard_main.ino
+++ Plant Sensor/plantSense_ard_main.ino
@@ -2,23 +2,67 @@ int IRpin = 1; //Infrared sensor pin
 int MSpin = 0; //moisture sensor pin
 char receivedChar;  //variable to store info from Rasp Pi
 boolean newData = false;
-
-//If Arduino is used for LED output
-//int greenLED = 13;
-//int redLED = 12; 
+const unsigned int MAX_INPUT = 50;
 
 void setup() {  
-  Serial.begin(9600); // open serial port, set the baud rate as 9600 bps
-  // If arduino is used for LED output
-  //pinMode(greenLED, OUTPUT);
-  //pinMode(redLED, OUTPUT);
+  Serial.begin(9600);
+}
+
+void process_data (const char * data) {
+  Serial.println("hit process_data");
+}
+
+void processIncomingByte (const byte inByte) {
+  static char input_line [MAX_INPUT];
+  static unsigned int input_pos = 0;
+
+  switch (inByte) {
+
+    case '\n':
+      input_line [input_pos] = 0;
+      process_data (input_line);
+      input_pos = 0;  
+      break;
+
+    case '\r':
+      break;
+
+    default:
+      if (input_pos < (MAX_INPUT - 1))
+        input_line [input_pos++] = inByte;
+      break;
+  }
+}
+
+// Serial check
+void recvInfo() {
+  while (Serial.available() > 0) {
+    processIncomingByte(Serial.read());
+//    receivedChar = Serial.read();
+    newData = true;
+  }
+}
+
+// Send sensor readings to RPi
+void RPi_Serial(float a, int b) {
+  if (newData == false) {
+    Serial.print(a);
+    Serial.print(" ");
+    Serial.println(b);
+  } else {
+    int distance = GetDistance();
+    newData = false;
+    Serial.print(receivedChar, DEC);
+    Serial.print(" ");
+    Serial.println(distance);
+  }
 }
 
 float GetMoisture() {
-  float dry = (analogRead(MSpin));  // connect sensor to Analog 0
-  float reading_min = 300;          // Minimum value of sensor
-  dry = (dry-reading_min)/reading_min;      // Set dry to be a percentage of dryness
-  float moist = 1.00 - dry;         // Moisture is the inverse of dryness
+  float dry = (analogRead(MSpin));
+  float reading_min = 300;
+  dry = (dry-reading_min)/reading_min;
+  float moist = 1.00 - dry;   
   return moist;
 }
 
@@ -38,43 +82,10 @@ int GetDistance() {
   }
 }
 
-void RPi_Serial(float a, int b) {
-    Serial.print(a);
-    Serial.print(" ");
-    Serial.println(b);
-}
-
 void loop() {
+  recvInfo();
   float moisture = GetMoisture();
   int distance = GetDistance();
   RPi_Serial(moisture, distance);
   delay(500);
-
-  //recvInfo(); // If arduino is used for LED output
-  //lightLED(); // If arduino is used for LED output
 }
-
-// // If arduino is used for LED output
-//void recvInfo() {
-
-  //if (Serial.available() > 0) {
-    //receivedChar = Serial.read();
-    //newData = true;
-  //}
-//}
-
-// If arduino is used for LED output
-//void lightLED() {
-  //int led = (receivedChar - '0');
-  //while(newData == true) {
-    //digitalWrite(led, HIGH);
-    //delay(1000);
-    //digitalWrite(led, LOW);
-    //delay(1000);
-    //digitalWrite(led, HIGH);
-    //delay(1000);
-    //digitalWrite(led, LOW);
-    //delay(1000);
-    //newData = false;
-  //}
-//}
--- Plant Sensor/plantSense_rpi_main.py
+++ Plant Sensor/plantSense_rpi_main.py
@@ -2,7 +2,7 @@
 import serial
 import time
 import os
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO    # Can only be run on a RPi
 
 
 def ardOutput_tuple():
@@ -15,11 +15,7 @@ def ardOutput_tuple():
 def soilCheck(rasp_input):
     soil_level = rasp_input[0]
     soilCheck_out = 0
-    if soil_level > soil_threshold:
-        print("Soil Greater than Threshold")
-        soilCheck_out = 0
-    elif soil_level <= soil_threshold:
-        print("Soil Less than Threshold")
+    if soil_level <= soil_threshold:
         soilCheck_out = 1
     return soilCheck_out
 
@@ -27,17 +23,30 @@ def soilCheck(rasp_input):
 def distCheck(rasp_input):
     dist_level = rasp_input[1]
     distCheck_out = 0
-    if dist_level > dist_threshold:
-        print("User is Not in Proximity")
-        distCheck_out = 0
-    elif dist_level <= dist_threshold:
+    if dist_level == True:
         print("User in Proximity")
         distCheck_out = 1
     return distCheck_out
 
 
 def userCheck():
-    pass
+    check_attempt = 0
+    while check_attempt < 3:
+        os.system(bash_thirsty)
+        timeout = 0
+        check_attempt += 1
+        while timeout < 3:
+            water_check = ardOutput_tuple()
+            print(check_attempt, timeout, water_check)
+            if water_check[0] < soil_threshold:
+                timeout += 1
+                time.sleep(5)
+            else:
+                os.system(bash_satis)
+                timeout = 3
+                check_attempt = 3
+            
+        
 
 
 def main():
@@ -50,35 +59,31 @@ def driver():
         try:
             ard_out = ardOutput_tuple()
             soil_status = soilCheck(ard_out)
-            
-            if soil_status == 0:
-                continue
-            elif soil_status == 1:
-                dist_status = distCheck(ard_out)
-                if dist_status == 0:
-                    continue
-                elif dist_status == 1:
-                    pass
-                    
-                    
-            
-            print("Iteration: %d | Soil: %g | Distance: %g" % (dev_counter+1, ard_out[0], ard_out[1]))
+            print("#%d) M: %g | Prox: %g\n" % (dev_counter+1,
+                                               ard_out[0],
+                                               ard_out[1]))
             dev_counter += 1
+            if soil_status == True:
+                dist_status = distCheck(ard_out)
+                if dist_status == True:
+                    userCheck()
+                else:
+                    pass
         except IndexError:
             continue
 
-
-# GPIO Config
-GPIO_LED_red = 24
-GPIO_LED_green = 23
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(GPIO_LED_red, GPIO.OUT)
-GPIO.setup(GPIO_LED_green, GPIO.OUT)
+# # Can only be run on RPi
+# # GPIO Config 
+# GPIO_LED_red = 24
+# GPIO_LED_green = 23
+# GPIO.setwarnings(False)
+# GPIO.setmode(GPIO.BCM)
+# GPIO.setup(GPIO_LED_red, GPIO.OUT)
+# GPIO.setup(GPIO_LED_green, GPIO.OUT)
 
 # 
-ser = serial.Serial('/dev/ttyACM0', 9600, 8, 'N', 1, timeout=3)
-audio_prefix = "aplay --format=S16_LE --rate=16000 " + os.getcwd()
+ser = serial.Serial('/dev/ttyS0', 9600, 8, 'N', 1, timeout=3)
+audio_prefix = "".join(["aplay --format=S16_LE --rate=16000 ", os.getcwd()]).replace("t ", "t\\ ")
 bash_thirsty = audio_prefix + "/static_audio/thirsty.raw"
 bash_satis = audio_prefix + "/static_audio/satisfied_plant.raw"
 bash_watered = audio_prefix + "/static_audio/watered_plant.raw"
@@ -87,7 +92,12 @@ bash_watered = audio_prefix + "/static_audio/watered_plant.raw"
 dist_threshold = .15
 soil_threshold = .40
 
-time.sleep(4)
+
+def dev():
+    str_fig = "4"
+    ser.write(str_fig.encode("utf-8"))
+    ser.close
 
 
-driver()
+dev()
+# driver()
