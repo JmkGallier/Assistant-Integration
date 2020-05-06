f0d640774a6db3f5116c4be6f33f54563dbeefb8
Assistant-Integration RPi <jmkgallier@gmail.com>
2020-05-05 20:46:02 -0400

    switched to explicit path declaration

--- Press-Sense Calendar/velo_detect.py
+++ Press-Sense Calendar/velo_detect.py
@@ -1,22 +1,28 @@
 #!/usr/bin/python3
 
-import serial
-import time
+# import serial
+# import time
 import os
-
-ser = serial.Serial('/dev/ttyACM0', 9600, 8, 'N', 1, timeout=3)
+# 
+# ser = serial.Serial('/dev/ttyACM0', 9600, 8, 'N', 1, timeout=3)
+pwd = os.getcwd()
 RPI_project_id = "pi-pillbox-ca671"
 RPI_model_id = "rasp-pillbox-46290"
-bashcommand = "googlesamples-assistant-pushtotalk --project-id %s --device-model-id %s -i static_audio/date_query.raw" % (RPI_project_id, RPI_model_id)
+script_directory = os.path.dirname(os.path.realpath(__file__))
+audiofile_loc = os.path.join(script_directory, "date_query.raw").replace(" ", "\\ ")
+#command2 = os.path.join(script_directory, "!!!!!!!!!!!").replace(" ", "\\ ")
 
-pressed = False
-while not pressed:
-    my_str = ser.readline().decode("utf-8").replace("\r\n", "")
-    if my_str == "Pressed":
-        pressed = True
-        break
-    elif my_str == "Not Pressed":
-        print("Calendar has been pressed")
+bashcommand = "googlesamples-assistant-pushtotalk --project-id %s --device-model-id %s -i %s" % (RPI_project_id, RPI_model_id, audiofile_loc)
 
+# pressed = False
+# while not pressed:
+#     my_str = ser.readline().decode("utf-8").replace("\r\n", "")
+#     if my_str == "Pressed":
+#         pressed = True
+#         break
+#     elif my_str == "Not Pressed":
+#         print("Calendar has been pressed")
+
+# pressed = False
 os.system(bashcommand)
-pressed = False
+#print(audiofile_loc)
