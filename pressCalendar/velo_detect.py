3d82c192362bf53df68299dbc8ba05b90682605b
James Gallier <jmkgallier@gmail.com>
2020-05-11 18:32:39 -0400

    prepping for multi bash command call

--- pressCalendar/velo_detect.py
+++ pressCalendar/velo_detect.py
@@ -11,11 +11,19 @@ date_query_location = "static_audio/date_query.raw"
 
 # Declares absolute path
 script_directory = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.dirname(script_directory)
+AsstIntegration_Dir = os.path.dirname(script_directory + "/AsstIntegration")
 date_query_location = os.path.join(script_directory, date_query_location).replace(" ", "\\ ")
 
-bashcommand = "googlesamples-assistant-pushtotalk --project-id %s --device-model-id %s -i %s" % (RPI_project_id,
+bash_Assistant = "googlesamples-assistant-pushtotalk --project-id %s --device-model-id %s -i %s" % (RPI_project_id,
                                                                                                  RPI_model_id,
                                                                                                  date_query_location)
+bash_start_virtmic = ".%s -s virtmic_start" % AsstIntegration_Dir
+bash_play_virtmic = ".%s -s virtmic_play" % AsstIntegration_Dir
+
+#os.system(bashcommand)
+print(date_query_location)
+
 # ser = serial.Serial('/dev/ttyACM0', 9600, 8, 'N', 1, timeout=3)
 # pressed = False
 # while not pressed:
@@ -26,6 +34,3 @@ bashcommand = "googlesamples-assistant-pushtotalk --project-id %s --device-model
 #     elif my_str == "Not Pressed":
 #         print("Calendar has been pressed")
 # pressed = False
-
-#os.system(bashcommand)
-print(date_query_location)
